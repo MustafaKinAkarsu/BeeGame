3671f621697609114b43358fb8146493a284db22
Omer Faruk <faruk.omer2019@gmail.com>
2021-07-06 15:59:29 +0300

    Relative position is obtained

--- Assets/Scripts/LocalGlobal.cs
+++ Assets/Scripts/LocalGlobal.cs
@@ -10,29 +10,36 @@ public class LocalGlobal : MonoBehaviour
 
     GameObject SpawnRight(GameObject origin ,float distance)
     {
-        Vector3 reference = origin.transform.position;
-        reference = origin.transform.InverseTransformVector(reference);
+        Vector3 reference = origin.transform.position;      
         GameObject go = Instantiate(baseCube, new Vector3(reference.x + distance, reference.y, reference.z), Quaternion.identity);
+        go.transform.position = getRelativePosition(origin.transform, reference);
         return go;
     }
 
     GameObject SpawnUp(GameObject origin, float distance)
     {
         Vector3 reference = origin.transform.position;
-        reference = origin.transform.localPosition;
-
         GameObject go = Instantiate(baseCube, new Vector3(reference.x, reference.y, reference.z + distance), Quaternion.identity);
+        go.transform.position = getRelativePosition(origin.transform, reference);
         return go;
     }
+    public static Vector3 getRelativePosition(Transform origin, Vector3 position)
+    {
+        Vector3 distance = position - origin.position;
+        Vector3 relativePosition = Vector3.zero;
+        relativePosition.x = Vector3.Dot(distance, origin.right.normalized);
+        relativePosition.y = Vector3.Dot(distance, origin.up.normalized);
+        relativePosition.z = Vector3.Dot(distance, origin.forward.normalized);
+        Debug.Log(relativePosition);
+        return relativePosition;
+    }
 
-   
     void Start()
     {
         GameObject cube1 = Instantiate(baseCube, new Vector3(0,6,0), Quaternion.identity);
-        cube1 = SpawnRight(cube1, 7);
-        cube1.transform.Rotate(0f, 60.0f, 0.0f, Space.Self);
-        cube1 = SpawnUp(cube1, 7);
-        cube1 = SpawnRight(cube1, 10);
+        GameObject cube2 = Instantiate(baseCube, new Vector3(10, 6, 0), Quaternion.Euler(0,60,0));
+        Vector3 relativepos = transform.InverseTransformVector(cube2.transform.position);
+        GameObject cube3 = Instantiate(baseCube, relativepos, Quaternion.identity);
     }
 
 }
