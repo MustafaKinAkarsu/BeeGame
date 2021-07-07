7342a7e57ab95884f20685f860b3d0dcc9c74f88
dncmumtaz <dncmumtaz@gmail.com>
2021-07-07 13:42:11 +0300

    borunun ölçeği değiştirldi

--- Assets/Scripts/FlyController.cs
+++ Assets/Scripts/FlyController.cs
@@ -11,7 +11,7 @@ public class FlyController : MonoBehaviour
     float distanceTravelled;
 
     float xOffset, yOffset;
-    float maxDistance = 1;
+    float maxDistance = 0.5f;
 
     [SerializeField]
     float controllerSpeed = 5;
