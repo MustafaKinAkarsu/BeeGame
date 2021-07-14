10b24801445a8f4dce4ccf568d780e29f89535be
dncmumtaz <dncmumtaz@gmail.com>
2021-07-14 19:55:12 +0300

    vs

--- Assets/Scripts/FlyController.cs
+++ Assets/Scripts/FlyController.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 
 public class FlyController : MonoBehaviour
 {
+    public float flytime = 0;
+
     public static FlyController instance;
     public PathCreator pathCreator;
     public EndOfPathInstruction endOfPathInstruction;
     public float speed = 1;
     public float distanceTravelled;
-
     public float xOffset, yOffset;
     float maxDistance = 0.5f;
 
     Vector2 firstPressPos;
     Vector2 secondPressPos;
     Vector2 currentSwipe;
+    Vector3 desiredPoint;
 
     [SerializeField]
     float controllerSpeed = 115;
 
     void Start()
     {
+
         instance = this;
         if (pathCreator != null)
         {
@@ -30,80 +33,32 @@ public class FlyController : MonoBehaviour
         }
     }
 
-    public void Awake()
-    {
-
-    }
 
     void Update()
     {
         if (pathCreator != null)
         {
             distanceTravelled += speed * Time.deltaTime;
-            Vector3 desiredPoint = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+             
+            desiredPoint = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
             transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
 
-            //SwipeMouse(); 
-            SwipeTouch();
+           
+           // SwipeTouch();
             
 
             transform.position = desiredPoint;
             xOffset = Mathf.Clamp(xOffset, -maxDistance, maxDistance);
             yOffset = Mathf.Clamp(yOffset, -maxDistance, maxDistance);
-            desiredPoint = transform.TransformPoint(new Vector3(xOffset, yOffset, 0)); //Arıyı merkezden xOffset ve Yoffset uzaklığına taşır.
 
+            desiredPoint = transform.TransformPoint(new Vector3(xOffset, yOffset, 0)); //Arıyı merkezden xOffset ve Yoffset uzaklığına taşır.                    
             transform.position = desiredPoint;
+            SwipeMouse();
         }
 
     }
 
-    public void SwipeMouse()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //save began touch 2d point
-            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            //save ended touch 2d point
-            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-            //create vector from the two points
-            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
-
-            //normalize the 2d vector
-            currentSwipe.Normalize();
-
-            //swipe upwards
-            if (currentSwipe.y > 0  && currentSwipe.x > -0.9f && currentSwipe.x < 0.9f && yOffset < 0.4f)
-        {
-                Debug.Log("up swipe");
-                StartCoroutine(Fly("Up"));
-            }
-            //swipe down
-            if (currentSwipe.y < 0 && currentSwipe.x > -0.9f && currentSwipe.x < 0.9f && yOffset > -0.4f)
-        {
-                Debug.Log("down swipe");
-                StartCoroutine(Fly("Down"));
-            }
-            //swipe left
-            if (currentSwipe.x < 0 && currentSwipe.y > -0.9f && currentSwipe.y < 0.9f && xOffset > -0.4f)
-        {
-                Debug.Log("left swipe");
-                StartCoroutine(Fly("Left"));
-            }
-            //swipe right
-            if (currentSwipe.x > 0 && currentSwipe.y > -0.9f && currentSwipe.y < 0.9f && xOffset < 0.4f)
-        {
-                Debug.Log("right swipe");
-                StartCoroutine(Fly("Right"));
-            }
-        }
-
-    }
-
-    public float flytime = 0;
     private IEnumerator Fly(string wheretoFly)
     {
         float tempY = transform.position.y;
@@ -117,8 +72,7 @@ public class FlyController : MonoBehaviour
                 while (flytime < 0.39f)
                 {
                     flytime += Time.deltaTime;
-                    transform.position = new Vector3(transform.position.x, Mathf.Lerp(tempY, tempY + 0.5f, flytime / 0.39f), transform.position.z);
-                    
+                    transform.position = new Vector3(transform.position.x, Mathf.Lerp(tempY, tempY + 0.5f, flytime / 0.39f), transform.position.z);                  
                     yield return null;
                 }
                 break;
@@ -141,8 +95,7 @@ public class FlyController : MonoBehaviour
                 while (flytime < 0.39f)
                 {
                     flytime += Time.deltaTime;
-                    transform.position = new Vector3(Mathf.Lerp(tempx, tempx - 0.5f, flytime / 0.39f),transform.position.y , transform.position.z);
-
+                    transform.position = new Vector3(Mathf.Lerp(tempx, tempx - 0.5f, flytime / 0.39f),transform.position.y , transform.position.z);                    
                     yield return null;
                 }
                 break;
@@ -183,33 +136,81 @@ public class FlyController : MonoBehaviour
                 currentSwipe.Normalize();
 
                 //swipe upwards
-                if (currentSwipe.y > 0 && currentSwipe.x > -0.9f && currentSwipe.x < 0.9f && yOffset < 0.4f)
+                if (currentSwipe.y > 0 && currentSwipe.x > -0.4f && currentSwipe.x < 0.4f && yOffset < 0.4f)
              {
                     Debug.Log("up swipe");
                     StartCoroutine(Fly("Up"));
                 }
                 //swipe down
-                if (currentSwipe.y < 0 && currentSwipe.x > -0.9f && currentSwipe.x < 0.9f && yOffset > -0.4f)
+                if (currentSwipe.y < 0 && currentSwipe.x > -0.4f && currentSwipe.x < 0.4f && yOffset > -0.4f)
              {
                     Debug.Log("down swipe");
                     StartCoroutine(Fly("Down"));
                 }
                 //swipe left
-                if (currentSwipe.x < 0 && currentSwipe.y > -0.9f && currentSwipe.y < 0.9f && xOffset > -0.4f)
+                if (currentSwipe.x < 0 && currentSwipe.y > -0.4f && currentSwipe.y < 0.4f && xOffset > -0.4f)
              {
                     Debug.Log("left swipe");
                     StartCoroutine(Fly("Left"));
                 }
                 //swipe right
-                if (currentSwipe.x > 0 && currentSwipe.y > -0.9f && currentSwipe.y < 0.9f && xOffset < 0.4f)
+                if (currentSwipe.x > 0 && currentSwipe.y > -0.4f && currentSwipe.y < 0.4f && xOffset < 0.4f)
              {
                     Debug.Log("right swipe");
                     StartCoroutine(Fly("Right"));
                 }
             }
         }
+
+
+
     }
 
+    public void SwipeMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            //swipe upwards
+            if (currentSwipe.y > 0 && currentSwipe.x > -0.4f && currentSwipe.x < 0.4f && yOffset < 0.4f)
+            {
+                Debug.Log("up swipe");
+                StartCoroutine(Fly("Up"));
+            }
+            //swipe down
+            if (currentSwipe.y < 0 && currentSwipe.x > -0.4f && currentSwipe.x < 0.4f && yOffset > -0.4f)
+            {
+                Debug.Log("down swipe");
+                StartCoroutine(Fly("Down"));
+            }
+            //swipe left
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.4f && currentSwipe.y < 0.4f && xOffset > -0.4f)
+            {
+                Debug.Log("left swipe");
+                StartCoroutine(Fly("Left"));
+            }
+            //swipe right
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.4f && currentSwipe.y < 0.4f && xOffset < 0.4f)
+            {
+                Debug.Log("right swipe");
+                StartCoroutine(Fly("Right"));
+            }
+        }
+
+    }
 
 
     // If the path changes during the game, update the distance travelled so that the follower's position on the new path
@@ -218,4 +219,7 @@ public class FlyController : MonoBehaviour
     {
         distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
     }
+
+
+
 }
